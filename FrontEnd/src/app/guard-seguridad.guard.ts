d3bd2fb323a26c42597f45763a912432099a41ac
portfolioWeb-Angular <rosales.gaston1@gmail.com>
2022-08-13 14:13:39 -0300

    developer to main

--- FrontEnd/src/app/guard-seguridad.guard.ts
+++ FrontEnd/src/app/guard-seguridad.guard.ts
@@ -1,15 +1,25 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { TokenService } from './servicios/token.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GuardSeguridadGuard implements CanActivate {
+
+  constructor(private tokenService:TokenService, private router:Router){}
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    if (this.tokenService.getToken()){
+      return true;
+    }
+    else {
+      this.router.navigate(['/']);
+      return false;
+    }
   }
   
 }
